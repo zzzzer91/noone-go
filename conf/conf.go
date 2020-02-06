6ef4556bfe032bed040ee64c5043d92f647550a8
zzzzer <zzzzer91@gmail.com>
2020-02-06 18:31:15 +0800

    v0.9.8 初步添加用户管理

--- conf/conf.go
+++ conf/conf.go
@@ -7,25 +7,22 @@ import (
 
 type SSConf struct {
 	Server       string            `json:"server"`
-	ServerPort   uint16            `json:"server_port"`
+	ServerPort   int               `json:"server_port"`
 	Password     string            `json:"password"`
 	Timeout      int               `json:"timeout"`
 	Method       string            `json:"method"`
 	PortPassword map[string]string `json:"port_password"`
 }
 
-var SS *SSConf
-
-func LoadJson(path string) error {
+func LoadJson(path string) (SSConf, error) {
+	var conf SSConf
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
-		return err
+		return conf, err
 	}
-	var conf SSConf
 	err = json.Unmarshal(b, &conf)
 	if err != nil {
-		return err
+		return conf, err
 	}
-	SS = &conf
-	return nil
+	return conf, nil
 }
