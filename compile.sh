b6fc6caedc72f2171f639770f798b651edfaceaf
zzzzer <zzzzer91@gmail.com>
2020-02-07 11:18:56 +0800

    v0.9.8 更新 compile.sh

--- compile.sh
+++ compile.sh
@@ -9,6 +9,12 @@ if [ ! -d "${BIN_DIR}" ]; then
   mkdir "${BIN_DIR}"
 fi
 
-echo "编译中..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "${BIN_DIR}/noone" "${CMD_DIR}/main.go"
-echo '编译完成！'
+echo '编译中...'
+# -w 去掉调试信息，得到的程序就不能用 gdb 调试了
+# -s 去掉符号表，panic 时候的 stack trace 就没有任何文件名/行号信息了，这个等价于普通 C/C++ 程序被 strip 的效果
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o "${BIN_DIR}/noone" "${CMD_DIR}/main.go"
+if [ $? -ne 0 ]; then
+  echo '编译失败！'
+else
+  echo '编译成功！'
+fi
